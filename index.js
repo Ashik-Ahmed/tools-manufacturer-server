eff29731da16d8c461d5a60299664c63e302ca6d
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-05-23 00:17:12 +0600

    verify jwt function added

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// pass = hZmTk7ap3vahiDMx
-// user = toolhousedbuser
 const express = require('express');
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -17,6 +15,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        console.log('Decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m12jl.mongodb.net/?retryWrites=true&w=majority`;
