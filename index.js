2bc2fd242da70a4b37d25f5476499c99710c5fa7
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-05-22 21:37:34 +0600

    add review api implemented

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ObjectId = require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 const app = express();
@@ -25,7 +26,24 @@ async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("tool-house").collection("tools");
+        const reviewCollection = client.db("tool-house").collection("reviews");
+        const userCollection = client.db("tool-house").collection("users");
 
+
+        // add a new user 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+
+            const updatedDoc = {
+                $set: user,
+            };
+
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
 
         //get all products
         app.get('/tools', async (req, res) => {
@@ -53,6 +71,51 @@ async function run() {
             res.send(tool);
         })
 
+
+        // payment api
+        app.post("/create-payment-intent", async (req, res) => {
+            const order = req.body;
+            const price = order.price;
+            const amount = price * 100;
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"]
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        // update paid status 
+        app.patch('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const payment = req.body;
+
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId,
+                }
+            }
+
+            const updatedOrder = await toolsCollection.updateOne(filter, updatedDoc);
+            res.send(updatedDoc);
+        })
+
+
+
+
+        // add a new review 
+        app.post('/addReview', async (req, res) => {
+            const newReview = req.body;
+            const result = await reviewCollection.insertOne(newReview);
+            res.send(result)
+        });
 
 
     }
